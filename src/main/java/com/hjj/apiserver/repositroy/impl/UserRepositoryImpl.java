50d648dcc9ddfef50f999ef21d4b98822e348d8f
hjj1991 <hjj19911@naver.com>
2022-05-08 17:02:47 +0900

    - 유저Repository 임포트 에러 수정

--- src/main/java/com/hjj/apiserver/repositroy/impl/UserRepositoryImpl.java
+++ src/main/java/com/hjj/apiserver/repositroy/impl/UserRepositoryImpl.java
@@ -1,18 +1,12 @@
 package com.hjj.apiserver.repositroy.impl;
 
-import com.hjj.apiserver.domain.QUserEntity;
-import com.hjj.apiserver.domain.QUserLogEntity;
 import com.hjj.apiserver.domain.UserEntity;
-import com.hjj.apiserver.dto.QUserDto_ResponseUserDetails;
-import com.hjj.apiserver.dto.UserDto;
 import com.hjj.apiserver.repositroy.UserRepositoryCustom;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Optional;
-
-import static com.hjj.apiserver.domain.QUserEntity.*;
-import static com.hjj.apiserver.domain.QUserLogEntity.*;
+import static com.hjj.apiserver.domain.QUserEntity.userEntity;
+import static com.hjj.apiserver.domain.QUserLogEntity.userLogEntity;
 
 @RequiredArgsConstructor
 public class UserRepositoryImpl implements UserRepositoryCustom {
