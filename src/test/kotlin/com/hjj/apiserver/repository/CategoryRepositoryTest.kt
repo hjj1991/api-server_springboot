3ea764f9b15d967cdda49de0ccff677ac6eaa12f
hjj1991 <hjj19911@naver.com>
2023-07-05 00:24:25 +0900

    카테고리 레포지터리 테스트코드 작성완료

--- src/test/kotlin/com/hjj/apiserver/repository/CategoryRepositoryTest.kt
+++ src/test/kotlin/com/hjj/apiserver/repository/CategoryRepositoryTest.kt
@@ -4,12 +4,10 @@ import com.hjj.apiserver.domain.accountbook.AccountBook
 import com.hjj.apiserver.domain.accountbook.AccountBookUser
 import com.hjj.apiserver.domain.accountbook.AccountRole
 import com.hjj.apiserver.domain.category.Category
-import com.hjj.apiserver.dto.accountbook.response.AccountBookFindAllResponse
 import com.hjj.apiserver.repository.accountbook.AccountBookRepository
 import com.hjj.apiserver.repository.accountbook.AccountBookUserRepository
 import com.hjj.apiserver.repository.category.CategoryRepository
 import org.assertj.core.api.Assertions
-import org.assertj.core.groups.Tuple
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -34,53 +32,64 @@ class CategoryRepositoryTest : BaseRepositoryTest() {
         val accountBook =
             accountBookRepository.save(AccountBook(accountBookName = "testBookName", accountBookDesc = "testBookDesc"))
 
-
-        val accountBookUser = accountBookUserRepository.save(
+        accountBookUserRepository.save(
             AccountBookUser(
                 accountBook = accountBook, user = tester1, accountRole = AccountRole.OWNER,
                 backGroundColor = "#00000", color = "#11111"
             )
         )
-        val parentCategory1 = categoryRepository.save(Category(
-            categoryName = "부모카테고리1",
-            categoryDesc = "부모카테고리1 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-        ))
-        val parentCategory2 = categoryRepository.save(Category(
-            categoryName = "부모카테고리2",
-            categoryDesc = "부모카테고리2 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-        ))
-        val parentCategory3 = categoryRepository.save(Category(
-            categoryName = "부모카테고리3",
-            categoryDesc = "부모카테고리3 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-        ))
-        val category1 = categoryRepository.save(Category(
-            categoryName = "자식카테고리1",
-            categoryDesc = "자식카테고리1 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-            parentCategory = parentCategory1,
-        ))
-        val category2 = categoryRepository.save(Category(
-            categoryName = "자식카테고리2",
-            categoryDesc = "자식카테고리2 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-            parentCategory = parentCategory1,
-        ))
+        val parentCategory1 = categoryRepository.save(
+            Category(
+                categoryName = "부모카테고리1",
+                categoryDesc = "부모카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+            )
+        )
+        val parentCategory2 = categoryRepository.save(
+            Category(
+                categoryName = "부모카테고리2",
+                categoryDesc = "부모카테고리2 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+            )
+        )
+        val parentCategory3 = categoryRepository.save(
+            Category(
+                categoryName = "부모카테고리3",
+                categoryDesc = "부모카테고리3 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+            )
+        )
+        val category1 = categoryRepository.save(
+            Category(
+                categoryName = "자식카테고리1",
+                categoryDesc = "자식카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+                parentCategory = parentCategory1,
+            )
+        )
+        val category2 = categoryRepository.save(
+            Category(
+                categoryName = "자식카테고리2",
+                categoryDesc = "자식카테고리2 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+                parentCategory = parentCategory1,
+            )
+        )
 
-        val category3 = categoryRepository.save(Category(
-            categoryName = "자식카테고리3",
-            categoryDesc = "자식카테고리3 설명",
-            categoryIcon = "",
-            accountBook = accountBook,
-            parentCategory = parentCategory3,
-        ))
+        val category3 = categoryRepository.save(
+            Category(
+                categoryName = "자식카테고리3",
+                categoryDesc = "자식카테고리3 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+                parentCategory = parentCategory3,
+            )
+        )
 
 
         // when
@@ -89,27 +98,104 @@ class CategoryRepositoryTest : BaseRepositoryTest() {
 
         // then
         Assertions.assertThat(findCategories.size).isEqualTo(3)
+        Assertions.assertThat(findCategories[0].categoryNo).isEqualTo(parentCategory1.categoryNo)
+        Assertions.assertThat(findCategories[1].categoryNo).isEqualTo(parentCategory2.categoryNo)
+        Assertions.assertThat(findCategories[2].categoryNo).isEqualTo(parentCategory3.categoryNo)
+        Assertions.assertThat(findCategories[0].childCategories).extracting("categoryNo").contains(category1.categoryNo)
+        Assertions.assertThat(findCategories[1].childCategories).isEmpty()
+        Assertions.assertThat(findCategories[2].childCategories[0].categoryNo).isEqualTo(category3.categoryNo)
 
     }
 
 
     @Test
-    @DisplayName("가계부의 해당사용자의 권한 조회가 정상 성공한다.")
-    fun findAccountRole_success() {
+    @DisplayName("카테고리 권한이 있는 경우 정상 조회된다.")
+    fun findCategoryByAccountRoleTest_success() {
         // given
         val tester1 = createUser("tester1", "테스터1")
-        val accountBook1 =
+
+        val accountBook =
             accountBookRepository.save(AccountBook(accountBookName = "testBookName", accountBookDesc = "testBookDesc"))
-        val accountBookUser1 = accountBookUserRepository.save(
+        accountBookUserRepository.save(
             AccountBookUser(
-                accountBook = accountBook1, user = tester1, accountRole = AccountRole.OWNER,
+                accountBook = accountBook, user = tester1, accountRole = AccountRole.OWNER,
                 backGroundColor = "#00000", color = "#11111"
             )
         )
+        val parentCategory1 = categoryRepository.save(
+            Category(
+                categoryName = "부모카테고리1",
+                categoryDesc = "부모카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+            )
+        )
+        categoryRepository.save(
+            Category(
+                categoryName = "자식카테고리1",
+                categoryDesc = "자식카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+                parentCategory = parentCategory1,
+            )
+        )
+
 
         // when
-        val findAccountRole = accountBookUserRepository.findAccountRole(tester1.userNo!!, accountBook1.accountBookNo!!)
-        Assertions.assertThat(findAccountRole).isEqualTo(accountBookUser1.accountRole)
+        val findCategory = categoryRepository.findCategoryByAccountRole(
+            parentCategory1.categoryNo!!, accountBook.accountBookNo!!,
+            tester1.userNo!!
+        )
+
+
+        // then
+        Assertions.assertThat(findCategory).isEqualTo(parentCategory1)
+
+    }
+
+    @Test
+    @DisplayName("카테고리 권한이 GUEST 경우 조회가 되지 않는다.")
+    fun findCategoryByAccountRoleTest_fail_when_accountRole_is_GUEST() {
+        // given
+        val tester1 = createUser("tester1", "테스터1")
+
+        val accountBook =
+            accountBookRepository.save(AccountBook(accountBookName = "testBookName", accountBookDesc = "testBookDesc"))
+        accountBookUserRepository.save(
+            AccountBookUser(
+                accountBook = accountBook, user = tester1, accountRole = AccountRole.GUEST,
+                backGroundColor = "#00000", color = "#11111"
+            )
+        )
+        val parentCategory1 = categoryRepository.save(
+            Category(
+                categoryName = "부모카테고리1",
+                categoryDesc = "부모카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+            )
+        )
+        categoryRepository.save(
+            Category(
+                categoryName = "자식카테고리1",
+                categoryDesc = "자식카테고리1 설명",
+                categoryIcon = "",
+                accountBook = accountBook,
+                parentCategory = parentCategory1,
+            )
+        )
+
+
+        // when
+        val findCategory = categoryRepository.findCategoryByAccountRole(
+            parentCategory1.categoryNo!!, accountBook.accountBookNo!!,
+            tester1.userNo!!
+        )
+
+
+        // then
+        Assertions.assertThat(findCategory).isNull()
+
     }
 
 }
