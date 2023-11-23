24dbac60b216b381b2e6286e6be12504d0e380c7
hjj1991 <hjj19911@naver.com>
2023-11-23 21:25:41 +0900

    로그인, 회원가입 리펙토링 작업
    - 소셜계정, 기본계정 통합작업중

--- src/main/kotlin/com/hjj/apiserver/dto/oauth2/OAuth2Attribute.kt
+++ src/main/kotlin/com/hjj/apiserver/dto/oauth2/OAuth2Attribute.kt
@@ -2,53 +2,56 @@ package com.hjj.apiserver.dto.oauth2
 
 import com.hjj.apiserver.common.exception.ProviderNotFoundException
 import com.hjj.apiserver.domain.user.Provider
+import com.hjj.apiserver.dto.user.UserAttribute
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.security.oauth2.core.user.OAuth2User
 
-class OAuth2Attribute(
-    val provider: Provider,
-    val providerId: String,
-    val userEmail: String? = null,
-    val nickName: String,
-    val name: String? = null,
-    val picture: String? = null,
-) {
+class OAuth2Attribute(registrationId: String?, oAuth2User: DefaultOAuth2User, nameAttributeKey: String) : OAuth2User {
+    val oAuth2User: DefaultOAuth2User
+    val userAttribute: UserAttribute
 
-    companion object {
-
-        fun of(
-            registrationId: String?,
-            oAuth2User: OAuth2User,
-            userNameAttributeName: String
-        ): OAuth2Attribute {
-            when (registrationId) {
-                "naver" -> {
-                    val attribute = oAuth2User.attributes[userNameAttributeName] as LinkedHashMap<String, String>
-                    return OAuth2Attribute(
-                        provider = Provider.NAVER,
-                        providerId = attribute["id"] ?: throw IllegalStateException(),
-                        userEmail = attribute["email"],
-                        nickName = attribute["nickname"] ?: throw IllegalArgumentException(),
-                        name = attribute["name"],
-                        picture = attribute["profile_image"]
-                    )
-                }
-
-                "kakao" -> {
-                    val kakaoAccount = oAuth2User.attributes["kakao_account"] as LinkedHashMap<String, Any>
-                    val profile = kakaoAccount["profile"] as LinkedHashMap<String, Any>
-                    return OAuth2Attribute(
-                        provider = Provider.KAKAO,
-                        providerId = userNameAttributeName,
-                        userEmail = kakaoAccount["email"] as String,
-                        nickName = profile["nickname"] as String,
-                        name = kakaoAccount["name"] as String?,
-                        picture = profile["profile_image_url"] as String
-                    )
-                }
-
-                else -> throw ProviderNotFoundException()
+    init {
+        this.oAuth2User = oAuth2User
+        when (registrationId) {
+            "naver" -> {
+                val attribute = oAuth2User.attributes[nameAttributeKey] as LinkedHashMap<String, String>
+                userAttribute = UserAttribute(
+                    provider = Provider.NAVER,
+                    providerId = attribute ["id"] ?: throw IllegalStateException(),
+                    userEmail = attribute["email"],
+                    nickName = attribute["nickname"] ?: throw IllegalArgumentException(),
+                    picture = attribute["profile_image"]
+                )
             }
+
+            "kakao" -> {
+                val kakaoAccount = oAuth2User.attributes["kakao_account"] as LinkedHashMap<String, Any>
+                val profile = kakaoAccount["profile"] as LinkedHashMap<String, Any>
+                userAttribute = UserAttribute(
+                    provider = Provider.KAKAO,
+                    providerId = nameAttributeKey,
+                    userEmail = kakaoAccount["email"] as String,
+                    nickName = profile["nickname"] as String,
+                    picture = profile["profile_image_url"] as String
+                )
+            }
+
+            else -> throw ProviderNotFoundException()
         }
+    }
+
+
+    override fun getName(): String {
+        return oAuth2User.name
+    }
+
+    override fun getAttributes(): MutableMap<String, Any> {
+        return oAuth2User.attributes
+    }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return oAuth2User.authorities
     }
 
 
