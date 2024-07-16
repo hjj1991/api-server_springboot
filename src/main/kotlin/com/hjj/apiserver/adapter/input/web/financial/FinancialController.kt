eb83665ea35a0a55d2ccb6c527a423d2034226ce
hwangjaejeong <hjj19911@naver.com>
2024-07-16 23:35:24 +0900

    기본 정렬 제거

--- src/main/kotlin/com/hjj/apiserver/adapter/input/web/financial/FinancialController.kt
+++ src/main/kotlin/com/hjj/apiserver/adapter/input/web/financial/FinancialController.kt
@@ -8,9 +8,7 @@ import com.hjj.apiserver.domain.financial.JoinRestriction
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 import org.springframework.data.domain.SliceImpl
-import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
-import org.springframework.data.web.SortDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -27,10 +25,7 @@ class FinancialController(
         @RequestParam(required = false) financialProductType: FinancialProductType?,
         @RequestParam(required = false) financialProductName: String?,
         @RequestParam(required = false) depositPeriodMonths: String?,
-        @PageableDefault(page = 0, size = 20)
-        @SortDefault.SortDefaults(
-            SortDefault(sort = ["companyName"], direction = Sort.Direction.ASC),
-        ) pageable: Pageable,
+        @PageableDefault(page = 0, size = 20) pageable: Pageable,
     ): Slice<FinancialProductResponse> {
         val financialProductsWithPaginationInfo =
             this.getFinancialUseCase.getFinancialsWithPaginationInfo(
